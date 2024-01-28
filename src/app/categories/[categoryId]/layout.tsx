575cadd5ea5e79060b31e2362fb0b2c7f5623c05
piggydoughnut <piggydoughnut@gmail.com>
2024-01-29 10:39:31 +1300

    og update

--- src/app/categories/[categoryId]/layout.tsx
+++ src/app/categories/[categoryId]/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Postcardru.com - Categories",
-  description: "Categories",
+  title: "Postcardru.com: Categories",
+  description: "Virtual Postcards from the 90s.",
 };
 
 export default function RootLayout({
@@ -16,6 +16,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta property="og:title" content="PostcardRu.com: Virtual Postcards" />
+      <meta
+        property="og:description"
+        content="Virtual Postcards from the 90s"
+      />
+      <meta
+        property="og:image"
+        content="https://postcardru.com/_next/image?url=%2Fpostcardimages%2Flove_love%2F22.jpg&w=828&q=75"
+      />
+      <meta property="og:url" content="https://postcardru.com/" />
+      <meta property="og:type" content="website" />
       <body className={inter.className}>
         <div className="flex flex-col justify-center items-center max-w-[750px] mx-auto pb-6">
           {children}
