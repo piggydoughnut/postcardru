4ba587b46733d700387710a03f89ed967df436df
piggydoughnut <piggydoughnut@gmail.com>
2025-12-21 13:30:19 +1300

    mailgun update

--- src/helpers/email.ts
+++ src/helpers/email.ts
@@ -1,20 +1,29 @@
-import Mailgun from "mailgun.js";
-const formData = require("form-data");
+let cachedClient: any;
 
-export const sendEmail = async (email: string, postcardUrl: string) => {
+const getMailgunClient = async () => {
+  if (cachedClient) return cachedClient;
+
+  const Mailgun = (await import("mailgun.js")).default;
+  const formData = (await import("form-data")).default;
+
   const mailgun = new Mailgun(formData);
-
-  const DOMAIN = "postcardru.com";
-  const client = mailgun.client({
+  cachedClient = mailgun.client({
     username: "api",
     key: process.env.EMAIL_API_KEY ?? "",
     url: "https://api.eu.mailgun.net",
   });
-  console.log("Sending a postcard to an email: ", email);
+
+  return cachedClient;
+};
+
+export const sendEmail = async (email: string, postcardUrl: string) => {
+  const client = await getMailgunClient();
+
+  const DOMAIN = "postcardru.com";
   const data = {
-    from: "PostcardRu  <hello@postcardru.com>",
+    from: "PostcardRu <hello@postcardru.com>",
     to: email,
-    subject: "You received a postcaard",
+    subject: "You received a postcard",
     html: `Hello! <br/><br/>
 You've got a postcard. To view this postcard, click on the
 following link at anytime within the next 365 days
